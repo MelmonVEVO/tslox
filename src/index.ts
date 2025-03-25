8f162f149be9028017dcae7a2142ead7287f7e4c
melmon <dgd@melmon.dev>
2025-03-25 15:06:02 +0000

    pretty!

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,127 @@
 import { readFileSync } from 'fs';
 
 enum TokenType {
-  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
   // One or two character tokens.
-  BANG, BANG_EQUAL,
-  EQUAL, EQUAL_EQUAL,
-  GREATER, GREATER_EQUAL,
-  LESS, LESS_EQUAL,
+  BANG,
+  BANG_EQUAL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
   // Literals.
-  IDENTIFIER, STRING, NUMBER,
+  IDENTIFIER,
+  STRING,
+  NUMBER,
   // Keywords.
-  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
 
   EOF
-};
+}
+
+enum ExpressionType {
+  UNARY,
+  BINARY,
+  GROUPING,
+  OPERATOR
+}
 
 const KEYWORDS_MAP = {
-  "AND": TokenType.AND,
-  "CLASS": TokenType.CLASS,
-  "ELSE": TokenType.ELSE,
-  "FALSE": TokenType.FALSE,
-  "FUN": TokenType.FUN,
-  "FOR": TokenType.FOR,
-  "IF": TokenType.IF,
-  "NIL": TokenType.NIL,
-  "OR": TokenType.OR,
-  "PRINT": TokenType.PRINT,
-  "RETURN": TokenType.RETURN,
-  "SUPER": TokenType.SUPER,
-  "THIS": TokenType.THIS,
-  "TRUE": TokenType.TRUE,
-  "VAR": TokenType.VAR,
-  "WHILE": TokenType.WHILE
+  AND: TokenType.AND,
+  CLASS: TokenType.CLASS,
+  ELSE: TokenType.ELSE,
+  FALSE: TokenType.FALSE,
+  FUN: TokenType.FUN,
+  FOR: TokenType.FOR,
+  IF: TokenType.IF,
+  NIL: TokenType.NIL,
+  OR: TokenType.OR,
+  PRINT: TokenType.PRINT,
+  RETURN: TokenType.RETURN,
+  SUPER: TokenType.SUPER,
+  THIS: TokenType.THIS,
+  TRUE: TokenType.TRUE,
+  VAR: TokenType.VAR,
+  WHILE: TokenType.WHILE
+};
+
+interface BinaryExpr {
+  type: ExpressionType.BINARY;
+  symbols: [Expression, Token, Expression];
 }
+
+interface UnaryExpr {
+  type: ExpressionType.UNARY;
+  symbols: [Token, Expression];
+}
+
+interface GroupingExpr {
+  type: ExpressionType.GROUPING;
+  symbols: [Token, Expression, Token];
+}
+
+interface OperatorExpr {
+  type: ExpressionType.OPERATOR;
+  symbols: [Token];
+}
+
+type Expression = BinaryExpr | UnaryExpr | GroupingExpr | OperatorExpr
 
 interface Token {
   type: TokenType;
   lexeme: string;
   line: number;
-};
+}
 
-const isDigit = (c: string) => /\d/.test(c)
+const isDigit = (c: string) => /\d/.test(c);
 
-const isAlpha = (c: string) => /[a-zA-Z]/.test(c)
+const isAlpha = (c: string) => /[a-zA-Z]/.test(c);
+
+const isAlphanumeric = (c: string) => isDigit(c) || isAlpha(c);
 
 // Pretty prints a provided error message.
 const reportErr = (line: number, message: string) => {
   console.error(`[line ${line}] Error: ${message}`);
 };
 
-
+const parseTokens = (tokens: Token[]) => {
+};
 
 // Takes in a script, tokenises it and returns the resulting array.
 const scanTokens = (script: string): Token[] => {
-
   const tokens: Token[] = [];
   let startChar = 0;
   let currentChar = 0;
   let line = 1;
 
-  const eof = (): boolean => currentChar >= script.length;
+  const eof = () => currentChar >= script.length;
 
   const newToken = (type: TokenType) => {
     tokens.push({
@@ -69,32 +129,60 @@ const scanTokens = (script: string): Token[] => {
       lexeme: script.substring(startChar, currentChar),
       line
     });
-  }
+  };
 
   const matchNext = (expected: string): boolean => {
     if (eof()) return false;
     if (script[currentChar] != expected) return false;
     currentChar++;
     return true;
-  }
+  };
 
   const scanToken = () => {
     const character = script[currentChar++];
     switch (character) {
-      case '(': newToken(TokenType.LEFT_PAREN); break;
-      case ')': newToken(TokenType.RIGHT_PAREN); break;
-      case '{': newToken(TokenType.LEFT_BRACE); break;
-      case '}': newToken(TokenType.RIGHT_BRACE); break;
-      case ',': newToken(TokenType.COMMA); break;
-      case '.': newToken(TokenType.DOT); break;
-      case '-': newToken(TokenType.MINUS); break;
-      case '+': newToken(TokenType.PLUS); break;
-      case ';': newToken(TokenType.SEMICOLON); break;
-      case '*': newToken(TokenType.STAR); break;
-      case '!': newToken(matchNext('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
-      case '=': newToken(matchNext('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
-      case '<': newToken(matchNext('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
-      case '>': newToken(matchNext('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+      case '(':
+        newToken(TokenType.LEFT_PAREN);
+        break;
+      case ')':
+        newToken(TokenType.RIGHT_PAREN);
+        break;
+      case '{':
+        newToken(TokenType.LEFT_BRACE);
+        break;
+      case '}':
+        newToken(TokenType.RIGHT_BRACE);
+        break;
+      case ',':
+        newToken(TokenType.COMMA);
+        break;
+      case '.':
+        newToken(TokenType.DOT);
+        break;
+      case '-':
+        newToken(TokenType.MINUS);
+        break;
+      case '+':
+        newToken(TokenType.PLUS);
+        break;
+      case ';':
+        newToken(TokenType.SEMICOLON);
+        break;
+      case '*':
+        newToken(TokenType.STAR);
+        break;
+      case '!':
+        newToken(matchNext('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        newToken(matchNext('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+        break;
+      case '<':
+        newToken(matchNext('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        newToken(matchNext('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+        break;
       case '/':
         if (matchNext('/')) {
           // Comment that goes to the end of the line.
@@ -122,7 +210,11 @@ const scanTokens = (script: string): Token[] => {
       default:
         if (isDigit(character)) {
           while (isDigit(script[currentChar])) currentChar++;
-          if (currentChar + 1 < script.length && matchNext('.') && isDigit(script[currentChar + 1])) {
+          if (
+            currentChar + 1 < script.length &&
+            matchNext('.') &&
+            isDigit(script[currentChar + 1])
+          ) {
             currentChar++;
             while (isDigit(script[currentChar])) currentChar++;
           }
@@ -131,18 +223,21 @@ const scanTokens = (script: string): Token[] => {
         }
         if (isAlpha(character)) {
           while (isAlpha(script[currentChar])) currentChar++;
-          const keyword = script.substring(startChar, currentChar).toUpperCase();
-          if (keyword in KEYWORDS_MAP) newToken(KEYWORDS_MAP[keyword]); else newToken(TokenType.IDENTIFIER);
+          const keyword = script
+            .substring(startChar, currentChar)
+            .toUpperCase();
+          if (keyword in KEYWORDS_MAP) newToken(KEYWORDS_MAP[keyword]);
+          else newToken(TokenType.IDENTIFIER);
           break;
         }
         reportErr(line, 'Unexpected character.');
         break;
     }
-  }
+  };
 
   while (!eof()) {
-    scanToken();
     startChar = currentChar;
+    scanToken();
   }
 
   tokens.push({
@@ -152,8 +247,7 @@ const scanTokens = (script: string): Token[] => {
   });
 
   return tokens;
-}
-
+};
 
 // Interprets a provided script.
 const runScript = (script: string) => {
@@ -181,7 +275,7 @@ const repl = () => {
     runScript(command);
 
     process.stdout.write('lox> ');
-  })
+  });
 };
 
 const main = (args: string[]) => {
